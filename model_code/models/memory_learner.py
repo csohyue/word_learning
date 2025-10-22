3c6db60c0d7763824201aa22cc74fc9758c05d5a
Christine Yue <christineyue@mac.mynetworksettings.com>
2025-10-22 09:42:20 -0400

    restructure model file structure

---
+++ model_code/models/memory_learner.py
@@ -0,0 +1,19 @@
+from mbp.memory_buffer import WorkingLearningSpace
+
+
+class MemoryLearner(object):
+    """
+    CSWL model with memory integrated into it
+    Attributes:
+        working_learning_space   WorkingLearningSpace object that keeps a limited number of words
+        associations             Dictionary{<str> word: np.array<float> association values
+        meanings                 [<str> object] index of the meaning corresponds to index of association value
+        lexicon                  Dictionary{<str> word: [<str> meaning]}
+    """
+    def __init__(self, working_learning_space_size=10):
+        self.working_learning_space = WorkingLearningSpace(working_learning_space_size)
+        self.associations = {}
+        self.meanings = []
+        self.lexicon = {}
+        self.removals = 0
+        self.working_learning_space_size = working_learning_space_size
